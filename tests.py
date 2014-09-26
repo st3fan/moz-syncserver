a1511ee96b2aa20bddea303327d276dc3e7e6daf
Stefan Arentz <stefan@arentz.ca>
2014-09-26 16:08:59 -0400

    Porting lots of tests from the Python sync server

--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ import json
 import uuid
 import time
 import unittest
+import urllib
 import urllib2
 
 import hawk
@@ -23,13 +24,13 @@ def call_token_server(server, assertion):
     url = server + "/token/1.0/sync/1.5"
     r = requests.get(url, headers={"Authorization": "BrowserID " + assertion})
     r.raise_for_status()
-    return r.json()
+    return r.json(),r
 
 def call_mockmyid_server(email, audience):
     url = "http://127.0.0.1:8080/assertion"
     r = requests.get(url, params={"email":email, "audience":audience})
     r.raise_for_status()
-    return r.json().get("assertion")
+    return r.json().get("assertion"),r
 
 def get_object(token, collection_name, object_id):
     url = token["api_endpoint"] + "/storage/%s/%s" % (collection_name, object_id)
@@ -37,7 +38,7 @@ def get_object(token, collection_name, object_id):
     hawk_header = hawk.client.header(url, "GET", {"credentials": hawk_credentials, "ext":""})
     r = requests.get(url, headers={"Authorization":hawk_header["field"]})
     r.raise_for_status()
-    return r.json()
+    return r.json(),r
 
 def put_object(token, collection_name, o_id, o):
     url = token["api_endpoint"] + "/storage/%s/%s" % (collection_name, o_id)
@@ -45,7 +46,7 @@ def put_object(token, collection_name, o_id, o):
     hawk_header = hawk.client.header(url, "PUT", {"credentials": hawk_credentials, "ext":""})
     r = requests.put(url, headers={"Authorization":hawk_header["field"]}, data=json.dumps(o))
     r.raise_for_status()
-    return float(r.text)
+    return float(r.text),r
 
 def delete_object(token, collection_name, object_id):
     url = token["api_endpoint"] + "/storage/%s/%s" % (collection_name, object_id)
@@ -53,7 +54,7 @@ def delete_object(token, collection_name, object_id):
     hawk_header = hawk.client.header(url, "DELETE", {"credentials": hawk_credentials, "ext":""})
     r = requests.delete(url, headers={"Authorization":hawk_header["field"]})
     r.raise_for_status()
-    return r.json()
+    return r.json(),r
 
 def post_objects(token, collection_name, objects):
     url = token["api_endpoint"] + "/storage/%s" % collection_name
@@ -61,7 +62,7 @@ def post_objects(token, collection_name, objects):
     hawk_header = hawk.client.header(url, "POST", {"credentials": hawk_credentials, "ext":""})
     r = requests.post(url, headers={"Authorization":hawk_header["field"],"Content-Type":"application/json"}, data=json.dumps(objects))
     r.raise_for_status()
-    return r.json()
+    return r.json(),r
 
 def get_info_collections(token):
     url = token["api_endpoint"] + "/info/collections"
@@ -69,7 +70,7 @@ def get_info_collections(token):
     hawk_header = hawk.client.header(url, "GET", {"credentials": hawk_credentials, "ext":""})
     r = requests.get(url, headers={"Authorization":hawk_header["field"]})
     r.raise_for_status()
-    return r.json()
+    return r.json(),r
 
 def get_info_collection_counts(token):
     url = token["api_endpoint"] + "/info/collection_counts"
@@ -77,7 +78,25 @@ def get_info_collection_counts(token):
     hawk_header = hawk.client.header(url, "GET", {"credentials": hawk_credentials, "ext":""})
     r = requests.get(url, headers={"Authorization":hawk_header["field"]})
     r.raise_for_status()
-    return r.json()
+    return r.json(),r
+
+def get_objects(token, collection_name, full=None, newer=None, limit=None, ids=None):
+    params={}
+    if full:
+        params["full"] = "1"
+    if newer:
+        params["newer"] = ("%.2f" % newer)
+    if limit:
+        params["limit"] = limit
+    if ids:
+        params["ids"] = ",".join(ids)
+    url = token["api_endpoint"] + "/storage/%s" % collection_name + "?" + urllib.urlencode(params)
+    hawk_credentials = {"id": str(token["id"]), "key": str(token["key"]), "algorithm":"sha256"}
+    hawk_header = hawk.client.header(url, "GET", {"credentials": hawk_credentials, "ext":""})
+    r = requests.get(url, headers={"Authorization":hawk_header["field"]})
+    r.raise_for_status()
+    return r.json(),r
+
 
 def random_id():
     return str(uuid.uuid4())
@@ -85,19 +104,22 @@ def random_id():
 def random_object():
     return {"payload":"This is some payload at %f" % time.time()}
 
+def random_objects(n):
+    return [{"payload":"This is some payload at %f" % time.time(), "id":random_id()} for i in range(n)]
+
 class StorageTest(unittest.TestCase):
 
     def setUp(self):
         self.email = "%.4f@mockmyid.com" % time.time()
-        self.assertion = call_mockmyid_server(self.email, SERVER)
-        self.token = call_token_server(SERVER, self.assertion)
+        self.assertion,r = call_mockmyid_server(self.email, SERVER)
+        self.token,r = call_token_server(SERVER, self.assertion)
 
     def test_put_object_partial(self):
         # Put a new object
-        modified = put_object(self.token, "things", "testid1", {"payload":"testpayload1"})
+        modified,r = put_object(self.token, "things", "testid1", {"payload":"testpayload1"})
         self.assertTrue(modified != 0)
         # Fetch it and see if it is correctly filled in
-        object = get_object(self.token, "things", "testid1")
+        object,r = get_object(self.token, "things", "testid1")
         self.assertTrue(object is not None)
         self.assertTrue(object["payload"] == "testpayload1")
         self.assertTrue(object["sortindex"] == DEFAULT_SORTINDEX)
@@ -107,10 +129,10 @@ class StorageTest(unittest.TestCase):
 
     def test_put_object_full(self):
         # Put a new object
-        modified = put_object(self.token, "things", "testid2", {"payload":"testpayload2", "sortindex":100, "ttl": 3600})
+        modified,r = put_object(self.token, "things", "testid2", {"payload":"testpayload2", "sortindex":100, "ttl": 3600})
         self.assertTrue(modified != 0)
         # Fetch it and see if it is correctly filled in
-        object = get_object(self.token, "things", "testid2")
+        object,r = get_object(self.token, "things", "testid2")
         self.assertTrue(object is not None)
         self.assertTrue(object["payload"] == "testpayload2")
         self.assertTrue(object["sortindex"] == 100)
@@ -120,12 +142,12 @@ class StorageTest(unittest.TestCase):
 
     def test_put_object_update(self):
         # Put a new object
-        modified = put_object(self.token, "things", "testid3", {"payload":"testpayload3", "sortindex":100, "ttl": 3600})
+        modified,r = put_object(self.token, "things", "testid3", {"payload":"testpayload3", "sortindex":100, "ttl": 3600})
         self.assertTrue(modified != 0)
         # Put it again with updated fields
-        modified = put_object(self.token, "things", "testid3", {"payload":"testpayload3updated"})
+        modified,r = put_object(self.token, "things", "testid3", {"payload":"testpayload3updated"})
         # Fetch it and see if it is correctly filled in
-        object = get_object(self.token, "things", "testid3")
+        object,r = get_object(self.token, "things", "testid3")
         self.assertTrue(modified != 0)
         self.assertTrue(object is not None)
         self.assertTrue(object["payload"] == "testpayload3updated")
@@ -136,31 +158,31 @@ class StorageTest(unittest.TestCase):
 
     def test_delete_object_existing(self):
         # Put a new object
-        modified = put_object(self.token, "things", "testid4", {"payload":"testpayload3", "sortindex":100, "ttl": 3600})
+        modified,r = put_object(self.token, "things", "testid4", {"payload":"testpayload3", "sortindex":100, "ttl": 3600})
         self.assertTrue(modified != 0)
         # Delete it
-        delete_object(self.token, "things", "testid4")
+        r = delete_object(self.token, "things", "testid4")
 
     def test_delete_object_unknown_collection(self):
         # Delete it with bad collection
         with self.assertRaises(requests.exceptions.HTTPError) as context:
-            delete_object(self.token, "thingzzz", "testid4")
+            r = delete_object(self.token, "thingzzz", "testid4")
         self.assertEqual(context.exception.response.status_code, 404)
 
     def test_delete_object_unknown_object(self):
         # Delete it with bad collection
         with self.assertRaises(requests.exceptions.HTTPError) as context:
-            delete_object(self.token, "things", "testid4zzz")
+            r = delete_object(self.token, "things", "testid4zzz")
         self.assertEqual(context.exception.response.status_code, 404)
 
     # Tests from server-syncstorage/syncstorage/tests/functional/test_storage.py
 
     def test_get_info_collections(self):
         # Create two collections
-        modified1 = put_object(self.token, "col1", random_id(), random_object())
-        modified2 = put_object(self.token, "col2", random_id(), random_object())
+        modified1,r = put_object(self.token, "col1", random_id(), random_object())
+        modified2,r = put_object(self.token, "col2", random_id(), random_object())
         # Only those two should be in /info/collections
-        collections = get_info_collections(self.token)
+        collections,r = get_info_collections(self.token)
         self.assertEqual(len(collections), 2)
         self.assertEqual(collections["col1"], modified1)
         self.assertEqual(collections["col2"], modified2)
@@ -172,10 +194,100 @@ class StorageTest(unittest.TestCase):
         for i in range(0,5):
             put_object(self.token, "col2", random_id(), random_object())
         # Only those two should be in /info/collection_counts
-        collections = get_info_collection_counts(self.token)
+        collections,r = get_info_collection_counts(self.token)
         self.assertEqual(len(collections), 2)
         self.assertEqual(collections["col1"], 3)
         self.assertEqual(collections["col2"], 5)
+
+    def test_bad_cache(self):
+        put_object(self.token, "col1", random_id(), random_object())
+        collections1,r = get_info_collections(self.token)
+        put_object(self.token, "col2", random_id(), random_object())
+        collections2,r = get_info_collections(self.token)
+        self.assertEquals(len(collections2), len(collections1)+1)
+
+    def test_get_collection(self):
+        # Create a collection
+        for i in range(5):
+            put_object(self.token, "col1", str(i), random_object())
+            #post_objects(self.token, "col1", bsos)
+        # Non-existent collections appear as empty
+        objects,r = get_objects(self.token, "doesnotexist")
+        self.assertEquals(objects, [])
+        # Get the object ids
+        object_ids,r = get_objects(self.token, "col1")
+        object_ids.sort()
+        self.assertEquals(object_ids, ['0', '1', '2', '3', '4'])
+        self.assertEquals(int(r.headers.get("X-Weave-Records", 0)), 5)
+
+    def test_get_collection_ids(self):
+        # Test ids
+        for i in range(5):
+            put_object(self.token, "col1", str(i), random_object())
+        object_ids,r = get_objects(self.token, "col1", ids=["1","3","17"])
+        object_ids.sort()
+        self.assertEquals(object_ids, ["1", "3"])
+        self.assertEquals(int(r.headers["X-Weave-Records"]), 2)
+
+    def test_newer(self):
+        # Test newer
+        ts1,r = put_object(self.token, "test", "id1", random_object())
+        ts2,r = put_object(self.token, "test", "id2", random_object())
+        self.assertTrue(ts1 != 0)
+        self.assertTrue(ts2 != 0)
+        self.assertTrue(ts1 < ts2)
+
+        object_ids,r = get_objects(self.token, "test", newer=ts1)
+        self.assertEquals(object_ids, ['id2'])
+
+        object_ids,r = get_objects(self.token, "test", newer=ts2)
+        self.assertEquals(object_ids, [])
+
+        object_ids,r = get_objects(self.token, "test", newer=(ts1-1))
+        self.assertEquals(sorted(object_ids), ['id1', 'id2'])
+
+    def test_full(self):
+        put_object(self.token, "test", random_id(), random_object())
+        put_object(self.token, "test", random_id(), random_object())
+
+        objects,r = get_objects(self.token, "test", full=True)
+        self.assertTrue(isinstance(objects, list))
+        self.assertEquals(len(objects), 2)
+        self.assertEquals(int(r.headers["X-Weave-Records"]), 2)
+
+        for o in objects:
+            for wanted in ('id', 'modified', 'payload'):
+                self.assertTrue(wanted in o)
+
+    # def test_get_collection_sort(self):
+    #     for i in range(10):
+    #         put_object(self.token, "test", str(i), {"payload":"foo", "sortindex":i})
+    #     objects,r = get_objects(self.token, "test", full=True)
+    #     self.assertEquals(len(objects), 10)
+    #     for i in range(10):
+    #         self.assertEquals(objects[i]["sortindex"], i)
+
+    def test_get_collection_ids_limit(self):
+        for i in range(5):
+            put_object(self.token, "test", str(i), random_object())
+        object_ids,r = get_objects(self.token, "test", limit=2)
+        self.assertEquals(int(r.headers["X-Weave-Next-Offset"]), 2)
+        self.assertEquals(len(object_ids), 2)
+        self.assertEquals(object_ids[0], "0")
+        self.assertEquals(object_ids[1], "1")
+
+    def test_get_collection_full_limit(self):
+        for i in range(5):
+            put_object(self.token, "test", str(i), random_object())
+        objects,r = get_objects(self.token, "test", limit=2, full=1)
+        self.assertEquals(int(r.headers["X-Weave-Next-Offset"]), 2)
+        self.assertEquals(len(objects), 2)
+        self.assertEquals(objects[0]["id"], "0")
+        self.assertEquals(objects[1]["id"], "1")
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
